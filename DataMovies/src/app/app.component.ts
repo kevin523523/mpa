c9bea9e6a5283433eabcf4b2ecc6b3595e94e2a3
Adrián Litardo <lalitard@espol.edu.ec>
2023-11-29 10:45:11 -0500

    modyfing components

--- DataMovies/src/app/app.component.ts
+++ DataMovies/src/app/app.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './shared/navbar/navbar.component'
+import { FooterComponent } from './shared/footer/footer.component'
+import { IndexComponent } from './pages/index/index.component'
+import { AboutComponent } from './pages/about/about.component'
+
+
+
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, NavbarComponent, FooterComponent, IndexComponent, AboutComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- DataMovies/src/app/app.routes.ts
+++ DataMovies/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { IndexComponent } from './pages/index/index.component';
+import { AboutComponent } from './pages/about/about.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: IndexComponent },
+    { path: 'about', component: AboutComponent },
+];
