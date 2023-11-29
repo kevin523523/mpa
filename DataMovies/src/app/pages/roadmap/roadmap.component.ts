b704d325064a1feb16f0b8f8104bbf5b45d7a7cc
Adrián Litardo <lalitard@espol.edu.ec>
2023-11-29 11:10:07 -0500

    creating other components

---
+++ DataMovies/src/app/pages/roadmap/roadmap.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-roadmap',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './roadmap.component.html',
+  styleUrl: './roadmap.component.css'
+})
+export class RoadmapComponent {
+
+}
