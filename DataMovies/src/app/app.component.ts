389e5816913a940acdb609d5c1ee4aa724052d9b
Adrián Litardo <lalitard@espol.edu.ec>
2023-12-04 10:59:12 -0500

    modyfing app component

--- DataMovies/src/app/app.component.ts
+++ DataMovies/src/app/app.component.ts
@@ -5,6 +5,13 @@ import { NavbarComponent } from './shared/navbar/navbar.component'
 import { FooterComponent } from './shared/footer/footer.component'
 import { IndexComponent } from './pages/index/index.component'
 import { AboutComponent } from './pages/about/about.component'
+import { HttpClientModule } from '@angular/common/http';
+
+//Importación de la interfaz
+import { Pelicula } from './interfaces/pelicula';
+
+//Importación del servicio
+import { DatosService } from './providers/datos.service'
 
 
 
@@ -13,10 +20,23 @@ import { AboutComponent } from './pages/about/about.component'
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, NavbarComponent, FooterComponent, IndexComponent, AboutComponent],
+  imports: [CommonModule, RouterOutlet, NavbarComponent, FooterComponent, IndexComponent, AboutComponent, HttpClientModule],
+  providers: [DatosService],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'DataMovies';
+  //Atributo con el tipo de dato de la interfaz
+  public data: Pelicula[] = [];
+  //Inyección de dependencia del servicio
+  constructor(private dataProvider: DatosService) { }
+  //Ejecución de la petición y suscripción de la respuesta
+
+  ngOnInit() {
+    this.dataProvider.getResponse().subscribe((response) => {
+      let dataArray = (response as Pelicula[]);
+      this.data = dataArray.slice(0, 10);
+    })
+  }
 }
--- DataMovies/src/app/providers/datos.service.ts
+++ DataMovies/src/app/providers/datos.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
 
+//Importación del HttpClient
+import { HttpClient } from '@angular/common/http';
+
 @Injectable({
   providedIn: 'root'
 })
 export class DatosService {
 
-  constructor() { }
+  //Atributo URL
+  private URL: string = 'https://datamovies-a687f-default-rtdb.firebaseio.com/collection';
+
+  constructor(private http: HttpClient) { }
+  //Método con la petición HTTP
+  getResponse() {
+    return this.http.get(this.URL);
+  }
 }
